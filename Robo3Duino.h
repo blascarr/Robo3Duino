10cf54db9158cd9d82a5526f026107b81b6bbfe6
Blascarr <adrian.blascarr@gmail.com>
2022-09-26 21:53:23 +0200

    Robo3Duino Review

--- Robo3Duino.h
+++ Robo3Duino.h
@@ -8,7 +8,7 @@
 
 Robo3Duino is a generic library useful for Advanced Robotics Applications.
 
-This library is part of a educational course to learn C++ in practice in https://github.com/blascarr/Robotic2Duino Couser or http://www.blascarr.com/ webpage.
+This library is part of a educational course to learn C++ in practice in https://github.com/blascarr/Robo3Duino Couser or http://www.blascarr.com/ webpage.
 
  *	
  *  This Library gives some helpful methods to manipulate transformation Matrix in order to create references
@@ -32,50 +32,274 @@ This library is part of a educational course to learn C++ in practice in https:/
 		#include "WProgram.h"
 	#endif
  	
+	typedef Matrix< 4, 4 > Pose3D;
 
-	//Point 3D Class for reference
+	//Matrix Auxiliar Functions
+
+	Pose2D rotx( float angle ){
+		float arrayRot[3][3] = {{1,0,0},{0,cos(angle/ang2rad),-sin(angle/ang2rad)},{0,sin(angle/ang2rad),cos(angle/ang2rad)}};
+		Pose2D m;
+		m = arrayRot;
+		return m;
+	}
+
+	Pose2D roty( float angle ){
+		float arrayRot[3][3] = {{cos(angle/ang2rad),0,sin(angle/ang2rad)},{0,1,0},{-sin(angle/ang2rad),0,cos(angle/ang2rad)}};
+		Pose2D m;
+		m = arrayRot;
+		return m;
+	}
+
+	Pose2D rotz( float angle ){	
+		float arrayRot[3][3] = {{cos(angle/ang2rad),-sin(angle/ang2rad),0},{sin(angle/ang2rad),cos(angle/ang2rad),0},{0,0,1}};
+		Pose2D m;
+		m = arrayRot;
+		return m;
+	}
+
+	Pose3D trotx( float angle ){
+		float arrayRot[4][4] = {{1,0,0,0},{0,cos(angle/ang2rad),-sin(angle/ang2rad),0},{0,sin(angle/ang2rad),cos(angle/ang2rad),0},{0,0,0,1}};
+		Pose3D m;
+		m = arrayRot;
+		return m;
+	}
+
+	Pose3D troty( float angle ){
+		float arrayRot[4][4] = {{cos(angle/ang2rad),0,sin(angle/ang2rad),0},{0,1,0,0},{-sin(angle/ang2rad),0,cos(angle/ang2rad),0},{0,0,0,1}};
+		Pose3D m;
+		m = arrayRot;
+		return m;
+	}
+
+	Pose3D trotz( float angle ){
+		float arrayRot[4][4] = {{cos(angle/ang2rad),-sin(angle/ang2rad),0,0},{sin(angle/ang2rad),cos(angle/ang2rad),0},{0,0,1,0},{0,0,0,1}};
+		Pose3D m;
+		m = arrayRot;
+		return m;
+	}
+
+	Pose3D transl( float px, float py, float pz ){
+		float arrayRot[4][4] = { {0,0,0,px}, {0,0,0,py}, {0,0,0,pz}, {0,0,0,1}};
+		Pose3D m;
+		m = arrayRot;
+		return m;
+	}
+
+ 	Pose2D rpy2r( float roll, float pitch, float yaw){
+		return rotx(roll)*roty(pitch)*rotz(yaw);
+	}
+
+	Pose2D eul2r( float phi, float theta, float psi){
+		return rotz(phi)*roty(theta)*rotz(psi);
+	}
+
+	Pose3D rpy2tr( float roll, float pitch, float yaw){
+		return trotx(roll)*troty(pitch)*trotz(yaw);
+	}
+
+	Pose3D eul2tr( float phi, float theta, float psi){
+		return trotz(phi)*troty(theta)*trotz(psi);
+	}
+
+	Pose3D se2(float px , float py, float pz , float roll, float pitch, float yaw ){
+    	Pose2D rot = rpy2r(roll, pitch, yaw);
+		Pose3D m = transl( px, py, pz );
+		m.Submatrix<3, 3>( 0 ,0 ) = rot;
+		return m;
+	}
+
+	/*Pose3D rot2tr(float angle){
+		
+		Matrix <4, 4, float> m = se2 (0, 0, angle);
+		return m;
+	
+	}
+
+	Pose3D trans2tr( float px , float py ){
+		
+		Matrix <4, 4, float> m = se2 (px, py, 0);
+		return m;
+	
+	}
+
+	Point3D Mt2Pt ( Pose3D m ){
+		
+		return Point3D (m(0, 2), m(1, 2));
+
+	}
+
+	Pose3D Pt2Mt ( Point3D p ){
+
+		//return se2( p.x , p.y , 0 )
+	}
+	*/
+
+	class Bot3DController {
+		public:
+			Pose3D T;
+			Bot3DController(void){
+				Bot3DController::T = se2(0,0,0,0,0,0);
+			};
+
+			Bot3DController(float px , float py , float pz, float roll, float pitch, float yaw){
+				Bot3DController::T = se2(px, py, pz, roll, pitch, yaw);
+			};
+
+			void Bot3DController::set( float px , float py , float pz, float roll, float pitch, float yaw ){
+				Bot3DController::T = se2(px, py, pz, roll, pitch, yaw);
+			};
+
+			Pose3D Bot3DController::move( float px , float py , float pz, float roll, float pitch, float yaw ){
+				//if (Pose3D::inv_x == -1) {px*=-1; angle*=-1;}
+				//if (Pose3D::inv_y == -1) {py*=-1;angle*=-1;}
+
+				Bot3DController::T *= se2(px, py, pz, roll, pitch, yaw);
+				return Bot3DController::T;
+			};
+
+			Pose3D Bot3DController::move( Pose3D m ){
+				//Inverse axis not defined
+				Bot3DController::T *= m;
+				return Bot3DController::T;
+			};
+
+			/*Pose3D(Pose3D *p){
+				Pose3D::m = p.m;
+				Pose3D::inv_x = p.inv_x;
+				Pose3D::inv_y = p.inv_y;
+			};*/
+
+			/*void Pose3D::setInv(bool mx, bool my, bool mz){
+				if (mx) inv_x=-1;
+				if (my) inv_y=-1;
+				if (mz) inv_z=-1;
+			};*/
+	};
+
+	class Bot3D : public Bot3DController{
+
+		public:
+
+			Bot3D(void) : Bot3DController( ){
+				
+			};
+
+			Pose3D Bot3D::fwd(float d){
+				Bot3D::move(d,0,0,0,0,0);
+				return Bot3D::T;
+			};
+
+			Pose3D Bot3D::back(float d){
+				Bot3D::move(-d,0,0,0,0,0);
+				return Bot3D::T;
+			};
+
+			Pose3D Bot3D::up(float d){
+				Bot3D::move(0,0,d,0,0,0);
+				return Bot3D::T;
+			};
+
+			Pose3D Bot3D::down(float d){
+				Bot3D::move(0,0,-d,0,0,0);
+				return Bot3D::T;
+			};
+
+			Pose3D Bot3D::left(float d){
+				Bot3D::move(0,-d,0,0,0,0);
+				return Bot3D::T;
+			};
+
+			Pose3D Bot3D::right(float d){
+				Bot3D::move(0,d,0,0,0,0);
+				return Bot3D::T;
+			};
+
+			Pose3D Bot3D::turnx(float angle){
+				Bot3D::move(0,0,0,angle,0,0);
+				return Bot3D::T;
+			};
+
+			Pose3D Bot3D::turny(float angle){
+				Bot3D::move(0,0,0,0,angle,0);
+				return Bot3D::T;
+			};
+
+			Pose3D Bot3D::turnz(float angle){
+				Bot3D::move(0,0,0,0,0,angle);
+				return Bot3D::T;
+			};
+
+			void Bot3D::print(){
+				Serial << " Pose :  " << Bot3D::T << "\n";
+			}
+	};
+
+	class Link3D{
+		public:
+			float d, a, theta, alpha;
+			bool sigma;
+			float m, G, B, offset;
+			// Pose3D T;
+			// r(3x1) I (3x3) Jm(3x3), qlim (2)
+
+			Link3D(){
+
+			}
+
+			Link3D( float d, float a, float alpha, float theta){
+				Link3D::d=d;
+				Link3D::a=a;
+				Link3D::alpha=alpha;
+				Link3D::theta=theta;
+			}
+
+			void set ( float d, float a, float alpha, float theta){
+				Link3D::d=d;
+				Link3D::a=a;
+				Link3D::alpha=alpha;
+				Link3D::theta=theta;
+			};
+
+	};
+
+	class SerialLink{
+		public:
+			Link3D* links = NULL;
+			int nlinks;
+
+			SerialLink( int nlinks ){
+				SerialLink::nlinks= nlinks;
+				SerialLink::links = new Link3D [nlinks];
+			}
+	};
+
+	// -------- Point 3D Class -----------//
 	class Point3D{
 		public:
 
-			float x, y,z;
-			uint16_t color;
+			vector3D p;
 
-			Point3D(void){
-				x = y = z = 0;
-			};
+			Point3D(void){};
 
 			Point3D(float x0, float y0,float z0){
-				x = x0;
-				y = y0;
-				z = z0;
-				color = 0x000000;
+				p(0) = x0;
+				p(1) = y0;
+				p(2) = z0;
 			};
 
-			Point3D(float x0, float y0, float z0, uint16_t color0){
-				x = x0;
-				y = y0;
-				z = z0;
-				color = color0;
-			};
-
-			void Point3D::set(float x0, float y0, float z0, uint16_t color0 = 0x000000){
-				x = x0;
-				y = y0;
-				z = z0;
-				color = color0;
+			void Point3D::set(float x0, float y0, float z0){
+				p(0) = x0;
+				p(1) = y0;
+				p(2) = z0;
 			};
 
 			void Point3D::move(float dx, float dy, float dz){
-				Point3D::x = Point3D::x + dx;
-				Point3D::y = Point3D::y + dy;
-				Point3D::z = Point3D::z + dz;
-
-				//Point3D::x = Point3D::x + inv_x*dx;
-				//Point3D::y = Point3D::y + inv_y*dy;
-				//Point3D::z = Point3D::z + inv_z*dz;
+				Point3D::p(0) += dx;
+				Point3D::p(1) += dy;
+				Point3D::p(2) += dz;
 			};
 
-			void Point3D::move(Matrix<3, 3, float> m){
+			void Point3D::move(Pose2D m){
 				Point3D::move(m(0,2),m(1,2),m(2,2));
 			};
 
@@ -104,221 +328,12 @@ This library is part of a educational course to learn C++ in practice in https:/
 			};
 	};
 
-	//Matrix Auxiliar Functions
-
-	Matrix<3,3, float> rotx( float angle ){
-		
-		float arrayRot[3][3] = {{1,0,0},{0,cos(angle/ang2rad),-sin(angle/ang2rad)},{0,sin(angle/ang2rad),cos(angle/ang2rad)}};
-		Matrix <3, 3, float> m (arrayRot);
-		return m;
-	
-	}
-
-	Matrix<3,3, float> roty( float angle ){
-		
-		float arrayRot[3][3] = {{cos(angle/ang2rad),0,sin(angle/ang2rad)},{0,1,0},{-sin(angle/ang2rad),0,cos(angle/ang2rad)}};
-		Matrix <3, 3, float> m (arrayRot);
-		return m;
-	
-	}
-
-	Matrix<3,3, float> rotz( float angle ){
-		
-		float arrayRot[3][3] = {{cos(angle/ang2rad),-sin(angle/ang2rad),0},{sin(angle/ang2rad),cos(angle/ang2rad),0},{0,0,1}};
-		Matrix <3, 3, float> m (arrayRot);
-		return m;
-	
-	}
-
-	Matrix<4,4, float> trotx( float angle ){
-		
-		float arrayRot[4][4] = {{1,0,0,0},{0,cos(angle/ang2rad),-sin(angle/ang2rad),0},{0,sin(angle/ang2rad),cos(angle/ang2rad),0},{0,0,0,1}};
-		Matrix <4, 4, float> m (arrayRot);
-		return m;
-	
-	}
-
-	Matrix<4,4, float> troty( float angle ){
-		
-		float arrayRot[4][4] = {{cos(angle/ang2rad),0,sin(angle/ang2rad),0},{0,1,0,0},{-sin(angle/ang2rad),0,cos(angle/ang2rad),0},{0,0,0,1}};
-		Matrix <4, 4, float> m (arrayRot);
-		return m;
-	
-	}
-
-	Matrix<4,4, float> trotz( float angle ){
-		
-		float arrayRot[4][4] = {{cos(angle/ang2rad),-sin(angle/ang2rad),0,0},{sin(angle/ang2rad),cos(angle/ang2rad),0},{0,0,1,0},{0,0,0,1}};
-		Matrix <4, 4, float> m (arrayRot);
-		return m;
-	
-	}
-
-	Matrix<4,4, float> transl( float px, float py, float pz ){
-		
-		float arrayRot[4][4] = { {0,0,0,px}, {0,0,0,py}, {0,0,0,pz}, {0,0,0,1}};
-		Matrix <4, 4, float> m (arrayRot);
-		return m;
-	
-	}
-
- 	Matrix<3,3, float> rpy2r( float roll, float pitch, float yaw){
-		
-		return rotx(roll)*roty(pitch)*rotz(yaw);
-	
-	}
-
-	Matrix<3,3, float> eul2r( float phi, float theta, float psi){
-		
-		return rotz(phi)*roty(theta)*rotz(psi);
-	
-	}
-
-	Matrix<4,4, float> rpy2tr( float roll, float pitch, float yaw){
-		
-		return trotx(roll)*troty(pitch)*trotz(yaw);
-	
-	}
-
-	Matrix<4,4, float> eul2tr( float phi, float theta, float psi){
-		
-		return trotz(phi)*troty(theta)*trotz(psi);
-	
-	}
-
-	Matrix<4,4, float> se2(float px , float py, float pz , float roll, float pitch, float yaw ){
-    	
-    	Matrix <3,3> rot = rpy2r(roll, pitch, yaw);
-		Matrix <4,4> m = transl( px, py, pz );
-		m.Submatrix(Range<3>(0),Range<3>(0)) = rot;
-		return m;
-	}
-
-	/*Matrix<4,4, float> rot2tr(float angle){
-		
-		Matrix <4, 4, float> m = se2 (0, 0, angle);
-		return m;
-	
-	}
-
-	Matrix<4, 4, float> trans2tr( float px , float py ){
-		
-		Matrix <4, 4, float> m = se2 (px, py, 0);
-		return m;
-	
-	}
-
-	Point3D Mt2Pt ( Matrix<4, 4, float> m ){
-		
-		return Point3D (m(0, 2), m(1, 2));
-
-	}
-
-	Matrix<4,4, float> Pt2Mt ( Point3D p ){
-
-		//return se2( p.x , p.y , 0 )
-	}
-	*/
-	class Pose3D{
-
+	class Pixel3D : public Point3D{
 		public:
-
-			Matrix<4, 4, float> m ;
-			int inv_x=1; 
-			int inv_y=1;
-			int inv_z=1;
-
-			Pose3D(void){
-				Pose3D::m = se2(0,0,0,0,0,0);
-			};
-
-			Pose3D(float px , float py , float pz, float roll, float pitch, float yaw){
-				Pose3D::m = se2(px, py, pz, roll, pitch, yaw);
-			};
-			
-			void Pose3D::set( float px , float py , float pz, float roll, float pitch, float yaw ){
-				Pose3D::m = se2(px, py, pz, roll, pitch, yaw);
-			};
-			
-			Matrix<4, 4, float> Pose3D::move( float px , float py , float pz, float roll, float pitch, float yaw ){
-				//if (Pose3D::inv_x == -1) {px*=-1; angle*=-1;}
-				//if (Pose3D::inv_y == -1) {py*=-1;angle*=-1;}
-
-				Pose3D::m = Pose3D::m*se2(px, py, pz, roll, pitch, yaw);
-				return Pose3D::m;
-			};
-
-			Matrix<4, 4, float> Pose3D::move( Matrix<4, 4, float> m ){
-				//Inverse axis not defined
-				Pose3D::m = Pose3D::m*m;
-				return Pose3D::m;
-			};
-
-			/*Pose3D(Pose3D *p){
-				Pose3D::m = p.m;
-				Pose3D::inv_x = p.inv_x;
-				Pose3D::inv_y = p.inv_y;
-			};*/
-
-			/*void Pose3D::setInv(bool mx, bool my, bool mz){
-				if (mx) inv_x=-1;
-				if (my) inv_y=-1;
-				if (mz) inv_z=-1;
-			};*/
-
-			
-
-			Matrix<4, 4, float> Pose3D::fwd(float d){
-				Pose3D::m = Pose3D::move(d,0,0,0,0,0);
-				return Pose3D::m;
-			};
-
-			Matrix<4, 4, float> Pose3D::back(float d){
-				Pose3D::m = Pose3D::move(-d,0,0,0,0,0);
-				return Pose3D::m;
-			};
-
-			Matrix<4, 4, float> Pose3D::up(float d){
-				Pose3D::m = Pose3D::move(0,0,d,0,0,0);
-				return Pose3D::m;
-			};
-
-			Matrix<4, 4, float> Pose3D::down(float d){
-				Pose3D::m = Pose3D::move(0,0,-d,0,0,0);
-				return Pose3D::m;
-			};
-
-			Matrix<4, 4, float> Pose3D::left(float d){
-				Pose3D::m = Pose3D::move(0,-d,0,0,0,0);
-				return Pose3D::m;
-			};
-
-			Matrix<4, 4, float> Pose3D::right(float d){
-				Pose3D::m = Pose3D::move(0,d,0,0,0,0);
-				return Pose3D::m;
-			};
-
-			Matrix<4, 4, float> Pose3D::turnx(float angle){
-				Pose3D::m = Pose3D::move(0,0,0,angle,0,0);
-				return Pose3D::m;
-			};
-
-			Matrix<4, 4, float> Pose3D::turny(float angle){
-				Pose3D::m = Pose3D::move(0,0,0,0,angle,0);
-				return Pose3D::m;
-			};
-
-			Matrix<4, 4, float> Pose3D::turnz(float angle){
-				Pose3D::m = Pose3D::move(0,0,0,0,0,angle);
-				return Pose3D::m;
-			};
-
-			void Pose3D::print(){
-				Serial.print(" Pose :  ");
-				Serial << Pose3D::m;
-				Serial.println();
-				
-			}
+			uint16_t color;
+			Pixel3D(float x0 = 0, float y0 = 0, float z0 = 0, uint16_t colour = 0 ): Point3D( x0, y0, z0 ), color( colour ){};
+			void setColor( uint16_t colour ) { color = colour; }
+			uint16_t getColor(){ return color; }
 	};
 
 	float distance(float x0, float y0, float z0, float xf, float yf, float zf){
@@ -326,16 +341,12 @@ This library is part of a educational course to learn C++ in practice in https:/
 	}
 
 	float distance(Point3D p0, Point3D pf){
-		return sqrt(pow(pf.x-p0.x,2)+pow(pf.y-p0.y,2)+pow(pf.z-p0.z,2));
+		return sqrt(pow(pf.p(0)-p0.p(0),2)+pow(pf.p(1)-p0.p(1),2)+pow(pf.p(2)-p0.p(2),2));
 	}
 
 	float distance(Pose3D P0, Pose3D Pf){
-		return sqrt( pow( Pf.m(0,2) - P0.m(0,2), 2 ) + pow( Pf.m(1,2) - P0.m(1,2) ,2) + pow(Pf.m(2,2) - P0.m(2,2) ,2) );
+		return sqrt( pow( Pf(0,2) - P0(0,2), 2 ) + pow( Pf(1,2) - P0(1,2) ,2) + pow(Pf(2,2) - P0(2,2) ,2) );
 	}
 	
-	class Link3D{
-
-
-	};
 
 #endif
